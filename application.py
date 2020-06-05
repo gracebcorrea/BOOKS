526c004b4d3687be802513da9f2307bad1850572
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-05 16:52:12 -0300

    Update application.py

--- application.py
+++ application.py
@@ -7,9 +7,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
+DATABASE_URL = "postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
+
 # Check for environment variable
 #if not os.getenv("DATABASE_URL"):
-if not os.getenv("postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"):
+if not os.getenv(DATABASE_URL):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -18,7 +20,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv(DATABASE_URL))
 db = scoped_session(sessionmaker(bind=engine))
 
 
