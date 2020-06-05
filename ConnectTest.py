c5b343cb1dc2bedf8760ce52c115e43e3994dab9
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-05 18:50:31 -0300

    Update ConnectTest.py

--- ConnectTest.py
+++ ConnectTest.py
@@ -1,14 +1,17 @@
 import p1db
 
 #connect to db
-con = p1db.connect({
+con = p1db.connect(
     host ="ec2-34-198-243-120.compute-1.amazonaws.com",
     database="d3ck6mm9jbc163",
     user="dgssjhgflgvwxj",
     password="b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740",
-    })
-#cursor
-cur = con.cursor()
+    )
+if not os.getenv(con):
+    raise RuntimeError("DATABASE_URL is not set")
+else:
+     print ("connected")
+
 
 
 
