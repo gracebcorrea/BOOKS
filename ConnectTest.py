ef7bb9b85ca7b66c904b617349a520b626f20f21
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-05 20:28:52 -0300

    Update ConnectTest.py

--- ConnectTest.py
+++ ConnectTest.py
@@ -1,31 +1,37 @@
 import os
-import db
+
+from flask import Flask, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# export DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
+
+# Check for environment variable
+#if not os.getenv("DATABASE_URL"):
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return "Project 1: TODO"
 
 
 
-#connect to db
-con = db.connect(
-    host = "ec2-34-198-243-120.compute-1.amazonaws.com",
-    database = "d3ck6mm9jbc163",
-    user = "dgssjhgflgvwxj",
-    password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740",
-    )
 
-#cursor
-cur = con.cursor()
-
-print ("Hello World")
-#execute query
-cur.execute("SELECT id, name from ConnectTest")
-
-print ("I´m connected")
-rows = cur.fechall()
-
-for row in rows:
-  print (f"id {row[0]} name {row[1]}")
-
-
-#close cursor
-cur.close()
-#close connection
-con.close()
+#tirar o debug no final
+if __name__ =="__main__":
+   app.run(debug=True)
+   main()
