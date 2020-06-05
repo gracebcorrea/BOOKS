1b805564906634d6b361ef335a58e9b9d345e2d0
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-05 15:36:29 -0300

    Initial commit

---
+++ application.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+#if not os.getenv("DATABASE_URL"):
+if not os.getenv("postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return "Project 1: TODO"
+
+
+
+
+#tirar o debug no final
+if __name__ =="__main__":
+   app.run(debug=True)
