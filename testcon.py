60ae249130efdd9f370f2895357bac1ae09c1dcb
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-06 17:44:53 -0300

    Update testcon.py

--- testcon.py
+++ testcon.py
@@ -5,23 +5,23 @@ con = psycopg2.connect(
       host = "ec2-34-198-243-120.compute-1.amazonaws.com",
       database= "d3ck6mm9jbc163",
       user = "dgssjhgflgvwxj",
-      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740"
-      )
+      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740")
+
 #cursor
 cur = con.cursor()
 
-#execute query
-cur.execute( "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;")
-Tables = cur.fetchall()
-for T in Tables:
-    print([T])
+#execute query see all tables on the db
+#cur.execute( "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY #table_name;")
+#Tables = cur.fetchall()
+#for T in Tables:
+#    print([T])
 
 
-cur.execute("SELECT  id, name FROM  (ConnectTest)")
+cur.execute("SELECT * FROM  public.ConnectTest WHERE table_schema = 'public';")
 Rows = cur.fetchall()
 for r in Rows:
-   print (f"id {r[0]}, name {[1]}")
-
+   #print (f"id {r[0]}, name {[1]}")
+    print([1])
 
 #close cursor
 cur.close()
