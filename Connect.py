96ef38fbf29827a97540cf8b6115133253e45733
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-06 15:08:54 -0300

    Update Connect.py

---
+++ Connect.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+#connect db
+con = psycopg2.connect(
+      host = "ec2-34-198-243-120.compute-1.amazonaws.com",
+      database= "d3ck6mm9jbc163",
+      user = "dgssjhgflgvwxj",
+      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740"
+      )
+#cursor
+cur = con.cursor()
+
+cur.execute("select id, name from connecttest")
+
+for r in rows:
+    print (f"id {r[0]}, name {[1]}")
+
+
+
+
+#close cursor
+cur.close()
+
+#close connection
+con.close()
