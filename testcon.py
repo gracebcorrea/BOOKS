16705cf22f13d6ab76276f6a7f9ab051f9f5e0b2
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-06 19:27:11 -0300

    Update testcon.py

--- testcon.py
+++ testcon.py
@@ -28,3 +28,10 @@ db.close()
 #set or export FLASK_APP=application.py
 #set or export FLASK_DEBUG=1
 #set or export DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
+
+
+#Api Key - Goodreads
+#Here is your developer key for using the Goodreads API. This key must be appended to every request using the form variable 'key'. (If you're using our write API, you'll need your secret too.)
+
+#key: vELE3rrO4BMGthbgfBiKA
+#secret: fLjGkGg3qi2ftPIlRJJK4gZkKlS8OV1uSnSh89x9Ak
