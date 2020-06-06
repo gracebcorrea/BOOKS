f189972c68c0efef26672adf9040b9179d3653ef
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-05 21:01:30 -0300

    Update ConnectTest.py

--- ConnectTest.py
+++ ConnectTest.py
@@ -7,11 +7,17 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-# export DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
+ # export DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
 
 # Check for environment variable
-#if not os.getenv("DATABASE_URL"):
-if not os.getenv("DATABASE_URL"):
+DATABASE_URL= (host:"ec2-34-198-243-120.compute-1.amazonaws.com",     
+               user: "dgssjhgflgvwxj",
+               password: "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740",
+               port: "5432",
+               database: "d3ck6mm9jbc163",
+               options: "")
+if not os.getenv(DATABASE_URL):
+#if not os.getenv(DATABASE_URL):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -20,8 +26,12 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv(DATABASE_URL)
+))
 db = scoped_session(sessionmaker(bind=engine))
+
+
+
 
 
 @app.route("/")
