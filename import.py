aba49bd094e0bb52c7628a02f48f623ead19e9ba
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-08 15:54:48 -0300

    Update import.py

--- import.py
+++ import.py
@@ -12,24 +12,17 @@ if not os.getenv("DATABASE_URL"):
 engine= create_engine(os.getenv("DATABASE_URL"))
 db =scoped_session(sessionmaker(bind=engine))
 
+
+
 def main():
-
-    f=open("books.csv")
-    reader =csv.reader(f)
-    for isbn,title,author,year in reader:
-        if year == "year":
-            print('skipped first line')
-        else:
-            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:a,:b,:c,:d)",
-            {"0":isbn,"1":title,"2":author,"3":year})
-
-    print("done")
-    db.commit()
-
-
-
-
-
+    f = open("books.csv", "r")  # needs to be opened during reading csv
+    reader = csv.reader(f)
+    next(reader)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+               {"isbn": isbn, "title": title, "author": author, "year": year})
+        db.commit()
+        print(f"Added book with ISBN: {isbn} Title: {title}  Author: {author}  Year: {year}")
 
 
 if __name__ == "__main__":
