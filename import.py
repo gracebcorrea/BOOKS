5a7c3a553d3e03a6da965fd2be528a0fd80e77f7
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-08 16:09:53 -0300

    Update import.py

--- import.py
+++ import.py
@@ -15,14 +15,14 @@ db =scoped_session(sessionmaker(bind=engine))
 #SH = Skip Header
 
 def main():
-    SH = open("books.csv", "r")
+    SH = open("books.csv")
     reader = csv.reader(SH)
     next(reader)
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
                {"isbn": isbn, "title": title, "author": author, "year": year})
         db.commit()
-        print(f"Added book with ISBN: {isbn} Title: {title}  Author: {author}  Year: {year}")
+        print(f"Added book : {isbn} Title: {title}  Author: {author}  Year: {year}")
 
 
 if __name__ == "__main__":
