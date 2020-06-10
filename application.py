32d6e380cd774f1969f8b7d8bf18d263c93412e4
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-10 17:32:22 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
-import os , requests, sqlalchemy, json 
+import os , requests, sqlalchemy, json
 from flask import Flask, session, render_template, request, redirect, url_for,Response,send_from_directory
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.security import generate_password_hash, check_password_hash
-from helper import login_required
-from PIL import Image
+
 
 
 app = Flask(__name__)
