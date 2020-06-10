a2614da6a92460ba291f3f37d7b280c84527a342
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-10 10:36:33 -0300

    Update application.py

--- application.py
+++ application.py
@@ -34,9 +34,11 @@ def index():
 # Login Page
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    with open('\templates\login.html') as f:
+         imgkit.from_file(f, '\images\img_avatar.jpg')
 
     return render_template("login.html")
-    imgkit.from_file("login.html", "../img_avatar.jpg"'")
+
 
     #if request.method == "POST":
     #    email = request.form.get("email")
