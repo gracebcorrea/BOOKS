1074de62c590a198e0b3b4f7bd78bc182c90580f
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-10 22:23:37 -0300

    Update application.py

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ def login():
     password = request.form.get("password")
     #check if the user exists on the base
     if db.execute("SELECT * FROM users WHERE username = :username",
-                {"username": username}).rowcount < 1:
+                {"username": username}).fetchone()  is not None:
         return render_template("Alerts.html", message="This user doesn´t exists, you need to register.")
     else:
         return render_template("login.html")
