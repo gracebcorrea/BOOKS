89659ccb213cd2231dfd58b4d62fb2f7795e1c24
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-11 20:48:08 -0300

    Update application.py

--- application.py
+++ application.py
@@ -61,25 +61,24 @@ def index():
 # Login Page
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    #return render_template("login.html")
+
     #receive form information)
     if request.method == 'POST':
-       username = request.form.get("username")
-       print(username)
-       password = request.form.get("password")
-       print(password)
-       rememberme = request.form.get("rememberme ")
-       print(rememberme)
-    #check if the user exists on the base
+        username = request.form.get("username")
+        print(username)
+        password = request.form.get("password")
+        print(password)
+        rememberme = request.form.get("rememberme ")
+        print(rememberme)
 
-       userchek=db.execute("SELECT * FROM users WHERE username=:username AND password=:password",
-                    {"username":username, "password":password}).fetchone()
-       if userchek is not None:
-           return render_template("Alerts.html", message="Wellcome, you are logged in!" )
-           return render_template("login.html")
-       else:
-           return render_template("Alerts.html", tipo="alert alert-danger", message="This Username is not here!")
+        #check if the user exists on the base
+        userchek=db.execute("SELECT * FROM users WHERE username=:username AND password=:password",{"username":username, "password":password}).fetchone()
+        if userchek is not None:
+            return render_template("Alerts.html", message="Wellcome, you are logged in!" )
 
+        else:
+            return render_template("Alerts.html", tipo="alert alert-danger", message="This Username is not here!")
+     return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
