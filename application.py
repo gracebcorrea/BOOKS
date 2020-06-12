43a16a75a98445e3d8b404bcd0dc10ccb8f670ac
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-12 19:17:57 -0300

    Update application.py

--- application.py
+++ application.py
@@ -76,8 +76,8 @@ def login():
        rememberme = request.form.get("rememberme")
        if db.execute("SELECT * FROM users WHERE username = :username and password = :password",
                  {"username": username} , {"password" : username}).rowcount >= 1:
-       return render_template("Alerts.html",tipo="alert alert-success", message="Wellcome , you are logged in!", username="username" )
-    else:
+            return render_template("Alerts.html",tipo="alert alert-success", message="Wellcome , you are logged in!", username="username" )
+     else:
          return render_template("Alerts.html",tipo="alert alert-danger" , message="This username or password not on database : " ,  username="username" )
          return("login.html")
 @app.route("/register", methods=["GET", "POST"])
