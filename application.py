ff7d423d3599c1d430cefd75a6ce31247781ee11
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-12 14:14:55 -0300

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import os , requests, sqlalchemy, json, psycopg2
+import os, requests, sqlalchemy, json, psycopg2
 from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -9,21 +9,29 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
-#DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
+#connect database on bash
+# source venv/Scripts/activate      (ativar ambiente virutal - toda vez que entrar no bash)
+# pip install requests    (verifica se tem atualizaçoes)
+# flask --version
+#set or export FLASK_APP=application.py
+#set or export FLASK_DEBUG=1
+#set or export DATABASE_URL="postgres://dgssjhgflgvwxj:b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740@ec2-34-198-243-120.compute-1.amazonaws.com:5432/d3ck6mm9jbc163"
 
-#connect db
-db = psycopg2.connect(
-      host = "ec2-34-198-243-120.compute-1.amazonaws.com",
-      database= "d3ck6mm9jbc163",
-      user = "dgssjhgflgvwxj",
-      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740")
+#or connect db
+#db = psycopg2.connect(
+#      host = "ec2-34-198-243-120.compute-1.amazonaws.com",
+#      database= "d3ck6mm9jbc163",
+#      user = "dgssjhgflgvwxj",
+#      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740")
 
 
 #cursor
-cur = db.cursor()
+#cur = db.cursor()
 
-# Check for environment variable
 
+
+
+# Check for environment variable - begin
 if not os.getenv("DATABASE_URL"):
    raise RuntimeError("DATABASE_URL is not set")
 
