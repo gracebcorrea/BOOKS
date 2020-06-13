aa5963607477cfaac606faed01316ab53797b894
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-13 19:06:44 -0300

    Update application.py

--- application.py
+++ application.py
@@ -91,20 +91,22 @@ def register():
        print("going to select:" , [username],[password],[ckpassword])
        #check if the user exists on the base
        if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 1:
-            print("user exists")
+            #print("user exists")
             return render_template("Alerts.html",tipo="alert alert-danger", message="This User is not new, try again or go to login page, ", username=username , NewUrl="/index")
             session.clear()
        elif password != ckpassword:
-            print("wrong pass" , [username],[password])
+            #print("wrong pass" , [username],[password])
             return render_template("Alerts.html",tipo="alert alert-danger", message="Passwords do no check, please try again, ", username=username , NewUrl="/register")
             session.clear()
        else:
             print("casastrar usuário")
-           #db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-           #        {"username": username, "password": password})
-           #db.commit()
-           #session["user"] = username #Store user id here
-           #session["logged"] = True
+            BEGIN;
+               db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password})
+ 
+            COMMIT;
+            db.commit()
+            session["user"] = username  #Store user id here
+            session["logged"] = True
             return render_template("Alerts.html",tipo="alert alert-success", message="You joined us with sucess:", username=username , NewUrl="/search")
     else:
         return render_template("register.html")
