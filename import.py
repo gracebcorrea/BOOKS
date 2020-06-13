2aca80433b604ddc44f9c744acf8e60e48b3bb1a
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-13 19:20:17 -0300

    Update import.py

--- import.py
+++ import.py
@@ -1,3 +1,12 @@
+#Import: Provided for you in this project is a file called books.csv, which is
+#a spreadsheet in CSV format of 5000 different books. Each one has an
+#ISBN number, a title, an author, and a publication year. In a Python file called
+#import.py separate from your web application, write a program that will take
+#the books and import them into your PostgreSQL database. You will first need
+#to decide what table(s) to create, what columns those tables should have, and
+#how they should relate to one another. Run this program by running python3
+#import.py to import the books into your database, and submit this program with
+# the rest of your project code.
 import os
 import csv
 from sqlalchemy import create_engine
