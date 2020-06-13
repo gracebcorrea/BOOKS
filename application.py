7c1b61f460229cc47788cc3099a98b7645190d2c
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-12 22:05:53 -0300

    Update application.py

--- application.py
+++ application.py
@@ -78,11 +78,11 @@ def login():
        print([username], [password])
        #check if the user exists on the base
        if db.execute("SELECT * FROM users WHERE username = :username and password = :password", {"username": username, "password": password}).rowcount == 1:
-           return render_template("Alerts.html",tipo="alert alert-success", message="Wellcome ", username=username , href="search")
+           return render_template("Alerts.html",tipo="alert alert-success", message="Wellcome ", username=username , local="search")
            #abrir seçao
 
        else:
-           return render_template("Alerts.html",tipo="alert alert-primary", message="This User or E-mail is not valid, please try again or join us", username=username , href="login")
+           return render_template("Alerts.html",tipo="alert alert-primary", message="This User or E-mail is not valid, please try again or join us", username=username , local="login")
 
 
     else:
