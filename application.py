fd2d2cedfa03ccaa927940ce46a67036b99d0d11
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-13 23:20:07 -0300

    Update application.py

--- application.py
+++ application.py
@@ -117,9 +117,9 @@ def register():
 def search():
     if session.get('user') is None:
         print("sessão search:" ,[session['user']], [session['logged']])
-        return render_template("Alerts.html",tipo="alert alert-danger", message="You are not logged, please login", username="session['user']" , NewUrl="/index")
+        return render_template("Alerts.html",tipo="alert alert-danger", message="You are not logged, please login", username=session['user'] , NewUrl="/index")
     else:
-        return render_template("search.html", Search="T", Bookspage="T", Login="F", NewUser="F", Logout="T",username="session['user']")
+        return render_template("search.html", Search="T", Bookspage="T", Login="F", NewUser="F", Logout="T",username=session['user'])
 
 
 # Review Page
