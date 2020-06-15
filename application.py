c040ab49ec9ab1645bc944e74363b182786f4a34
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-15 17:21:03 -0300

    Update application.py

--- application.py
+++ application.py
@@ -131,8 +131,9 @@ def search():
             results.append(result)
 
             for result in results:
-                print(result.title)
-                return render_template("search.html" , checkedvalue = checkedvalue, books=books, SQLquerry = SQLquerry , title = result.title, author = result.author, isbm = result.isbm, year = result.year)
+                print(result)
+                #return render_template("search.html" , checkedvalue = checkedvalue, SQLquerry = SQLquerry , title = result.title, author = result.author, isbm = result.isbm, year = result.year)
+                return render_template("search.html" , checkedvalue = checkedvalue, SQLquerry = SQLquerry , result = result)
 
             if len(results) == 0:
                 return render_template("Alerts.html", tipo="alert alert-danger", message="no results for this search",  NewUrl="/search")
