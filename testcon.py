a252b2963b7f03ea2359662d94fa2cb818741b4e
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-15 14:59:23 -0300

    Update testcon.py

--- testcon.py
+++ testcon.py
@@ -3,9 +3,10 @@ import psycopg2
 #connect db
 db = psycopg2.connect(
       host = "ec2-34-198-243-120.compute-1.amazonaws.com",
-      database= "d3ck6mm9jbc163",
       user = "dgssjhgflgvwxj",
-      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740")
+      password = "b7c2cd60be73f4127ca0dc1159d755dfebcf9881459a8885b2ec2ee4b2cf2740",
+      database= "d3ck6mm9jbc163"      
+      )
 
 #cursor
 cur = db.cursor()
