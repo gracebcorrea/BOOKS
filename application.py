f1b9f3cc3c041b1c9985cec00725da0d2a543f55
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-16 16:50:17 -0300

    Update application.py

--- application.py
+++ application.py
@@ -134,17 +134,18 @@ def search():
         SQLquerry = "Agatha Christie" #request.form.get("SQLquerry")
 
         #SQL= ("SELECT * FROM books WHERE (:checkedvalue ) = (:SQLquerry)",{"checkedvalue" :checkedvalue , "SQLquerry" : SQLquerry})
-        SQL = "SELECT id, title, author, isbm, year FROM public.books  WHERE [:checkedvalue] = [:SQLquerry]  "
+        SQL = "SELECT id, title, author, isbm, year FROM public.books  WHERE [:checkedvalue] = [:SQLquerry]"
         Parametros = "{'checkedvalue' : checkedvalue, 'SQLquerry': SQLquerry}"
-        if db.execute(SQL, Parametros).fetchall():
 
+        if db.execute(SQL, Parametros).fetchall():
+           i=0
            x = len(SQL)
-           for result in results:
+           while i <= len(items):
               results.append([id], [title], [author], [isbm],[year])
 
               print(x, result)
               return render_template("search.html" , checkedvalue = checkedvalue, SQLquerry = SQLquerry ,x = x, result = [result] )
-              x -= 1
+              i += 1
 
         else:
             return render_template("Alerts.html", tipo="alert alert-danger", message="no results for this search",  NewUrl="/search")
