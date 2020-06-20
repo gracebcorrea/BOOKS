cd087c6b847f1685efb4ecc6cacfc2794dcc4d15
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-20 13:16:07 -0300

    Update import.py

--- import.py
+++ import.py
@@ -12,17 +12,13 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
-
 
 engine= create_engine(os.getenv("DATABASE_URL"))
 db =scoped_session(sessionmaker(bind=engine))
 
 #SH = Skip Header
-
 def main():
     SH = open("books.csv")
     reader = csv.reader(SH)
